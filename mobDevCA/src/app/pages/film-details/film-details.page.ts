0a9a353c5df4b52d14d60fe7aff487eec0516fab
Mike Yang <msness16@gmail.com>
2023-05-09 17:27:37 +0100

    Add 1 movie details, retrieve from api

--- mobDevCA/src/app/pages/film-details/film-details.page.ts
+++ mobDevCA/src/app/pages/film-details/film-details.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-film-details',
   templateUrl: './film-details.page.html',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmDetailsPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+  
   ngOnInit() {
   }
+  clicked = false;
+  contents: any=[] ;
 
+  runHttp(){
+    this.http.get('https://www.omdbapi.com/?i=tt3896198&apikey=fc242de8')
+      .subscribe(data => {
+        console.log(data);
+        this.contents.push(data);
+      });
+      
+  }
+  
 }
